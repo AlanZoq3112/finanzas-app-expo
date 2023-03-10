0188c97980f9137a65a256ce51edb744385852c8
AlanZoq3112 <20213tn009@utez.edu.mx>
2023-03-10 08:40:15 -0600

    First Commit

---
+++ config/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqQGMbffmwnrsVZjUyTBF3Su-2eaqEuWQ",
+  authDomain: "finanzasmatiu.firebaseapp.com",
+  projectId: "finanzasmatiu",
+  storageBucket: "finanzasmatiu.appspot.com",
+  messagingSenderId: "871950995649",
+  appId: "1:871950995649:web:3301795dd8d294ad91c440"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
